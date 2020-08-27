92f5d60d8be589bdaaf3165e01645e4c57eaa91d
Nathan <68413475+NatG91@users.noreply.github.com>
2020-08-27 13:11:12 +0530

    Add files via upload

---
+++ Maze.js
@@ -0,0 +1,17 @@
+class Maze {
+
+  constructor(x,y,){
+      this.x=x
+      this.y=y
+      this.height=100
+      this.width=20
+  }
+  display(){
+
+  fill("grey")
+
+  rect(this.x,this.y,this.width,this.height,);
+      
+
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,64 @@
+var maze1,maze2,maze3,maze4,maze5,maz6,maze7
+,maze8,maze9,maze10,maze11,maze12,maze13,maze14,
+maze15,maze16,maze17,maze18,maze19,maze20
+
+
+function setup(){
+
+createCanvas(1600,800)
+
+maze1= new Maze(200,500)
+maze2= new Maze(200,400)
+maze3= new Maze(100,200)
+maze3= new Maze(200,300)
+maze4= new Maze(200,100)
+maze5= new Maze(200,200)
+maze6= new Maze(20,93)
+maze7= new Maze(200,200)
+maze8= new Maze(200,200)
+maze9= new Maze(200,200)
+maze10= new Maze(200,200)
+maze11= new Maze(200,200)
+maze12= new Maze(200,200)
+maze13= new Maze(200,200)
+maze14= new Maze(200,200)
+maze15= new Maze(200,200)
+maze16= new Maze(200,200)
+maze17= new Maze(200,200)
+maze18= new Maze(200,200)
+maze19= new Maze(200,200)
+maze20= new Maze(200,200)
+
+
+
+
+
+
+
+}
+
+function draw(){
+background("rgb(173,216,230)")
+
+maze1.display();
+maze2.display();
+maze3.display();
+maze4.display();
+maze5.display();
+maze6.display();
+maze7.display();
+maze8.display();
+maze9.display();
+maze10.display();
+maze11.display();
+maze12.display();
+maze13.display();
+maze14.display();
+maze15.display();
+maze16.display();
+maze17.display();
+maze18.display();
+maze19.display();
+maze20.display();
+
+}
